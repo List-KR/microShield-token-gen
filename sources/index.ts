0cad73d14b87a38bbb47ff53ad676084f0bd0bce
PiQuark6046 <j.w.so@adguard.com>
2024-08-01 23:08:12 +0000

    chore: typo

--- sources/index.ts
+++ sources/index.ts
@@ -2,7 +2,7 @@ import * as Commander from 'commander'
 import { PostCode } from './globalping.js'
 
 const Program = new Commander.Command()
-Program.option('--token <token>', 'Github token')
+Program.option('--token <token>', 'GitHub token')
 Program.option('--repo <repo>', 'Github repo path')
 Program.option('--config <config>', 'config file name')
 Program.parse(process.argv)
