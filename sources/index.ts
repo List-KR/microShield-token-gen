71eee3097d9c1a02a0a07d210f3e2ef627e24d15
PiQuark6046 <j.w.so@adguard.com>
2024-08-05 10:00:19 +0000

    Update npm dependencies and refactor code for capturing Ad-Shield remote token

--- sources/index.ts
+++ sources/index.ts
@@ -1,9 +1,6 @@
 import * as Commander from 'commander'
-import * as SimpleGit from 'simple-git'
-import PLimit from 'p-limit'
+import * as GitHub from '@octokit/rest'
 import * as Fs from 'node:fs'
-import * as Os from 'node:os'
-import { PostCode, GetResult, TContinent } from './globalping.js'
 import { webcrack } from 'webcrack'
 import { ExtractCode } from './analyze.js'
 import { StringToSHA1HEX } from './sha.js'
@@ -11,39 +8,21 @@ import { StringToSHA1HEX } from './sha.js'
 const Program = new Commander.Command()
 Program.option('--token <token>', 'GitHub token')
 Program.option('--repo <repo>', 'Github repo path')
-Program.option('--config <config>', 'config file name')
+Program.option('--target <config>', 'Targeted file')
 Program.parse(process.argv)
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const ProgramOptions = Program.opts() as { token: string, repo: string, config: string }
-const GitInstance = SimpleGit.simpleGit({ baseDir: ProgramOptions.repo })
+const ProgramOptions = Program.opts() as { token: string, repo: string, target: string }
 
-const RawConfig = Fs.readFileSync(`${ProgramOptions.repo}/${ProgramOptions.config}`, 'utf8')
-const Config = JSON.parse(RawConfig) as { Domains: string[], AdShieldDomains: string[], Contient: TContinent[] }
-
-const PLimitInstance = PLimit(Os.cpus().length)
-const PLimitJobs: Promise<void>[] = []
-
-async function Job(Domain: string, Contient: TContinent, AdShieldDomain: string) {
-  const Current = new Date()
-  const PostCodeResult = await PostCode(`https://${AdShieldDomain}/loader.min.js`, { referer: `https://${Domain}` }, Contient)
-  while ((await GetResult(PostCodeResult.id)).status !== 'finished') {
-    await new Promise((Resolve) => setTimeout(Resolve, 1000))
-  }
-  const RawCode = (await GetResult(PostCodeResult.id)).results[0].result.FinishedHttpTestResult.rawBody
-  Fs.writeFileSync(`/tmp/${PostCodeResult.id}`, RawCode)
-  const ExtractedCode = ExtractCode((await webcrack(Fs.readFileSync(`/tmp/${PostCodeResult.id}`, 'utf8'))).code)
-  Fs.writeFileSync(`${ProgramOptions.repo}/${Current.getFullYear()}/${Current.getMonth()}/${Current.getDate()}/${await StringToSHA1HEX(RawCode)}.token`, ExtractedCode)
-}
-
-for (let I = 0; I < Config.Domains.length; I++) {
-  for (let J = 0; J < Config.Contient.length; J++) {
-    for (let K = 0; K < Config.AdShieldDomains.length; K++) {
-      PLimitJobs.push(PLimitInstance(() => Job(Config.Domains[I], Config.Contient[J], Config.AdShieldDomains[K])))
-    }
-  }
-}
-
-await Promise.all(PLimitJobs)
-GitInstance.add('.')
-GitInstance.commit('Update at ' + new Date().toISOString())
-GitInstance.push()
+const Current = new Date()
+const GitHubInstance = new GitHub.Octokit({ auth: ProgramOptions.token })
+const RawCode = Fs.readFileSync(ProgramOptions.target, 'utf8')
+const SHA = await StringToSHA1HEX(RawCode)
+Fs.writeFileSync(`/tmp/${SHA}`, RawCode)
+const ExtractedToken = ExtractCode((await webcrack(Fs.readFileSync(`/tmp/${SHA}`, 'utf8'))).code)
+await GitHubInstance.repos.createOrUpdateFileContents({
+  owner: ProgramOptions.repo.split('/')[0],
+  repo: ProgramOptions.repo.split('/')[1],
+  path: `${Current.getUTCFullYear()}/${Current.getUTCMonth()}/${Current.getUTCDate()}/${SHA}.token`,
+  message: `Update for ${SHA}`,
+  content: btoa(ExtractedToken)
+})
